599c2638d88e940d2e99838f9b16104146a942f3
Mike Beach <mike.beach1@sainsburys.co.uk>
2020-01-24 09:28:27 +0000

    new workflow

--- send.sh
+++ send.sh
@@ -3,4 +3,4 @@ curl --request POST \
 --url https://api.sendgrid.com/v3/mail/send \
 --header "Authorization: Bearer $SENDGRID_API_KEY" \
 --header 'Content-Type: application/json' \
---data '{"personalizations": [{"to": [{"email": "me@mikebeach.co.uk"}]}],"from": {"email": "test@example.com"},"subject": "Send Timesheet Reminders out","content": [{"type": "text/plain", "value": "Remember to send timesheet meme too!"}]}'
+--data '{"personalizations": [{"to": [{"email": "mike.beach1@sainsburys.co.uk"}]}],"from": {"email": "test@example.com"},"subject": "Send Timesheet Reminders out","content": [{"type": "text/plain", "value": "Remember to send timesheet meme too!"}]}'

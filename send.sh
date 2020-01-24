42ad79e8c96cebc7d3e0521a7edc58197c0fe7a6
Mike Beach <mike.beach1@sainsburys.co.uk>
2020-01-24 09:16:51 +0000

    update messages

--- send.sh
+++ send.sh
@@ -3,4 +3,4 @@ curl --request POST \
 --url https://api.sendgrid.com/v3/mail/send \
 --header "Authorization: Bearer $SENDGRID_API_KEY" \
 --header 'Content-Type: application/json' \
---data '{"personalizations": [{"to": [{"email": "me@mikebeach.co.uk"}]}],"from": {"email": "test@example.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'
+--data '{"personalizations": [{"to": [{"email": "me@mikebeach.co.uk"}]}],"from": {"email": "test@example.com"},"subject": "Send Timesheet Reminders out","content": [{"type": "text/plain", "value": "Remember to send timesheet meme too!"}]}'

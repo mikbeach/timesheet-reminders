440a71ef81985898245500d17d1c05bb09f5d763
Mike Beach <mike.beach1@sainsburys.co.uk>
2020-01-24 09:09:28 +0000

    update export

--- send.sh
+++ send.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl --request POST \
 --url https://api.sendgrid.com/v3/mail/send \
---header "Authorization: Bearer ${{ SENDGRID_API_KEY }}" \
+--header "Authorization: Bearer $SENDGRID_API_KEY" \
 --header 'Content-Type: application/json' \
 --data '{"personalizations": [{"to": [{"email": "me@mikebeach.co.uk"}]}],"from": {"email": "test@example.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'

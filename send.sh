05a087f55648fc17493f01d4d8a75189edddf9de
Mike Beach <mike.beach1@sainsburys.co.uk>
2020-06-25 13:18:39 +0100

    update sh

--- send.sh
+++ send.sh
@@ -3,4 +3,4 @@ curl --request POST \
 --url https://api.sendgrid.com/v3/mail/send \
 --header "Authorization: Bearer $SENDGRID_API_KEY" \
 --header 'Content-Type: application/json' \
---data '{"personalizations": [{"to": [{"email": "mike.beach1@sainsburys.co.uk"}]}],"from": {"email": "timesheet.overlord@timsheet.com"},"subject": "Do your timehseet!","content": [{"type": "text/plain", "value": "Do your timesheet -- https://sainsburys.pvcloud.com/planview/Track/Time/"}]}'
+--data '{"personalizations": [{"to": [{"email": $MY_EMAIL}]}],"from": {"email": "timesheet.overlord@timsheet.com"},"subject": "Do your timehseet!","content": [{"type": "text/plain", "value": "Do your timesheet -- $TS_URL"}]}'
